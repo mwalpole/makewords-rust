13a0a83d02cec7b5e0975476fd724aff57c52aa5
Mark Walpole <mark.walpole.dev@gmail.com>
2022-01-22 00:01:06 -0500

    add choice of game but ugh

--- src/main.rs
+++ src/main.rs
@@ -1,12 +1,61 @@
+use std::env;
 use std::io;
 use std::cmp::Ordering;
+use filters::filter::Filter;
 use rand::Rng;
 
-fn main() {
-    let npick = rand::thread_rng().gen_range(1..101);
-    let mut turn = 1;
+// Capture and process args
+// Demonstrate filtering
+// Demonstrate test
 
-    println!("Please try to guess a number: {}", npick);
+struct EqualTo {
+    pub length: usize,
+}
+
+impl Filter<usize> for EqualTo {
+    fn filter(&self, n: &usize) -> bool {
+        self.length == *n
+    }
+}
+
+fn parse_args(args: &[String]) -> (Vec<&str>, usize) {
+    let words = &args[1];
+    let words: Vec<&str> = words
+        .split(",")
+        .collect();
+    
+    let length = &args[2];
+    let length: usize = match length.trim().parse() {
+        Ok(num) => num,
+        Err(_) => 0,
+    };
+        
+    (words, length)
+}
+
+fn word_length(words: Vec<&str>, length: usize) {
+
+    let n: usize = words.len();
+
+    println!("Demo");
+    println!("{} found: {:?}", n, words);
+
+    let eq = EqualTo { length: length };
+    let result: Vec<&str> = words
+        .into_iter()
+        .filter(|s| eq.filter(&s.len()))
+        .collect();
+    
+    println!("{} of length {}", result.len(), length);
+}
+
+fn word_guess(words: Vec<&str>) {
+    let n: usize = words.len();
+    let npick = rand::thread_rng().gen_range(1..n);
+    let answer = words[npick];
+    println!("Please guess the word: {}", answer);
+
+    let mut turn = 1;
 
     loop{        
         let mut guess = String::new();
@@ -15,7 +64,7 @@ fn main() {
             .read_line(&mut guess)
             .expect("Failed to read guess");
 
-        let guess: u32 = match guess.trim().parse() {
+        let guess: usize = match guess.trim().parse() {
             Ok(num) => num,
             Err(_) => continue,
         };
@@ -31,5 +80,18 @@ fn main() {
             }
         }
         turn += 1;
+    }
+}
+
+fn main() {
+    let args: Vec<String> = env::args().collect();
+
+    let (words, length) = parse_args(&args);
+    let zero: usize = 0;
+
+    match length.cmp(&zero) {
+        Ordering::Less => println!("Length must be positive integer or None"),
+        Ordering::Equal => word_guess(words),
+        Ordering::Greater => word_length(words, length)
     }
 }
